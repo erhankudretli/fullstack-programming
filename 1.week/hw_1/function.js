2b446ae94c1fb63da826c434a257180b66c1d576
Erhan Kudretli <erhankudretli@gmail.com>
2021-03-07 21:55:03 +0100

    added first homework

--- 1.week/hw_1/function.js
+++ 1.week/hw_1/function.js
@@ -1,1 +1,20 @@
-const deneme="eleman"
+function collect(a,b){
+return console.log("collection" + a+b);
+}
+function subs(a,b){
+return console.log("substruction" + a-b);
+ }
+function divise(a,b){
+return console.log("division" + a/b);
+}
+
+function mult(a,b){
+return console.log("multiplition" + a*b);
+ }
+                        
+module.exports={
+    collection:collect,
+    substruction:subs,
+    division:divise,
+    multiplition:mult
+}
--- 1.week/hw_1/index.js
+++ 1.week/hw_1/index.js
@@ -1,1 +1,16 @@
-const deneme="eleman"
+/*
+-user interaction 
+4 islemi barindiran bir ek modul yazmamiz istendi.
+- technical details
+4 islem icin 4 fonksiyon yazilir.
+function.js te module exports objesi olusturulur.
+index.js te require ile fonksiyonlarin adresi belirtilir.*/
+
+const {collect, subs, mult, divise} = require("./function"); 
+
+collect(12,3);
+subs(12,3);
+divise(12,3);
+mult(12,3);
+
+
