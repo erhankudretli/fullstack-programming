5600c0863af4864d01c9b1624c1c6194156f0167
Erhan Kudretli <erhankudretli@gmail.com>
2021-03-18 20:12:09 +0100

    added description and comment lines

--- 2.week/hw_2/read.js
+++ 2.week/hw_2/read.js
@@ -1,7 +1,18 @@
-const XLSX = require("xlsx");
-const workbook = XLSX.readFile("./Ornek.xlsx");
+/*
+--User interaction--
+    - Excel dosyasindaki veriler okunacak
+--Technical Details --
+    - kullanilacak paket tanimlanir 
+    - calisacak excel dosyasina ulasilir 
+    - cekmek istedigimiz sayfa tanimlanir.
+    - sayfa icerisindeki veriler json formatina cevrilir ve ekrana bastirilir.
+*/
 
-let ws=workbook.Sheets["Sheet1"];
-let ReadData=XLSX.utils.sheet_to_json(ws);
+
+const XLSX = require("xlsx"); // node package
+const workbook = XLSX.readFile("./Ornek.xlsx"); // üzerinde islem yapacagimiz dosya
+
+let ws=workbook.Sheets["Sheet1"]; // dosya icerisindeki sayfa secilir
+let ReadData=XLSX.utils.sheet_to_json(ws); // json a cevirilir
 console.log(ReadData);
 
--- 2.week/hw_2/write.js
+++ 2.week/hw_2/write.js
@@ -1,9 +1,73 @@
+/*
+--User interaction--
+    - Excel dosyasinda yeni bir sayfa acilacak ve oraya data.js icindeki datalar yazilacak
+--Technical Details --
+    - kullanilacak paket tanimlanir 
+    - Örnek dataya erisisim saglanir. ORNEK DATAYA ERISIM SAGLAYAMADIM DOLAYISIYLA O DATAYI ANA SAYFAYA CEKIP O SEKILDE YAPTIM
+    - calisacak excel dosyasina ulasilir 
+    - örnek data yapisi excel formatina cevrilir.
+    - excel icinde yeni bir sayfa olusturulur ve sheet formatindaki data buraya gönderilir.
+    - excel dosyasi kayit edilir.
+*/
+
+
 const XLSX = require("xlsx");
-const Sdata=require(`./data.js`) // sample data
+// const Sdata=require(`./data`) // sample data 
+let Sdata = [{
+        ISIM: 'Cabbar',
+        SOYISIM: 'Mikail',
+        YAS: 22,
+        'ALDIGI MAAS': 6000,
+        CINSIYETI: 'ERKEK'
+    },
+    {
+        ISIM: 'Hans',
+        SOYISIM: 'Joe',
+        YAS: 39,
+        'ALDIGI MAAS': 16000,
+        CINSIYETI: 'ERKEK'
+    },
+    {
+        ISIM: 'Murtaza',
+        SOYISIM: 'Kaya',
+        YAS: 49,
+        'ALDIGI MAAS': 6000,
+        CINSIYETI: 'ERKEK'
+    },
+    {
+        ISIM: 'Marion',
+        SOYISIM: 'Minna',
+        YAS: 55,
+        'ALDIGI MAAS': 9000,
+        CINSIYETI: 'KADIN'
+    },
+    {
+        ISIM: 'Murat',
+        SOYISIM: 'Burhan',
+        YAS: 40,
+        'ALDIGI MAAS': 10000,
+        CINSIYETI: 'ERKEK'
+    },
+    {
+        ISIM: 'Abdurrezzak',
+        SOYISIM: 'Adigüzel',
+        YAS: 22,
+        'ALDIGI MAAS': 6000,
+        CINSIYETI: 'ERKEK'
+    },
+    {
+        ISIM: 'Mehmet',
+        SOYISIM: 'Sökmen',
+        YAS: 33,
+        'ALDIGI MAAS': 12000,
+        CINSIYETI: 'ERKEK'
+    },
+
+]
 
 const workbook = XLSX.readFile("./Ornek.xlsx"); // excell dosyasi
 
 const SdataJson = XLSX.utils.json_to_sheet(Sdata) // sample data excel formatina cevrildi
 
-XLSX.utils.book_append_sheet(workbook,SdataJson,"NewSheet") // workbook dosyasinda NewSheet isminde bir sayfa olustu ve Sdata Json buraya eklendi
-XLSX.writeFile(workbook,'Ornek.xlsx') // eklenen ve olusturuan bilgiler kaydedildi. 
+XLSX.utils.book_append_sheet(workbook, SdataJson, "NewSheet") // workbook dosyasinda NewSheet isminde bir sayfa olustu ve Sdata Json buraya eklendi
+XLSX.writeFile(workbook, './Ornek.xlsx') // eklenen ve olusturuan bilgiler kaydedildi. 
