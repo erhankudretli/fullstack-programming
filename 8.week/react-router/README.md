852fe71d8aa8b73fe7eff9c55722cadb6ecdc75d
Erhan Kudretli <erhankudretli@gmail.com>
2021-05-11 13:29:41 +0100

    added 8th week homework react-router

---
+++ 8.week/react-router/README.md
@@ -0,0 +1,21 @@
+# React Router
+
+
+-React router usage with a nawbar.
+
+-Unit Test with Jest
+
+
+
+
+## Installation
+
+-Starting-
+
+npm start 
+
+
+
+-Test-
+
+npm test
---
+++ 8.week/react-router/src/components/home.js
@@ -0,0 +1,12 @@
+import React  from "react";
+import img from './img1.jpg';
+
+
+export default function Home(){
+  return <div className="home">
+    <h2>Sitemize Hosgeldin. Formu Doldurmak icin ilgili linke tikla </h2>
+    <img src={img} alt="form"/>
+  </div>
+}
+
+
